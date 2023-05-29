b5b7e7de2e034f27f98916ef38fc9c1b9c7fc217
koayenay <koayenay@gmail.com>
2023-05-29 10:51:55 -0700

    worked on Product component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Product from "./components/Product"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+
+function App() {
+  return (
+    <div className='App'>
+      <Product />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Product.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react"
+import { Card, Button } from "react-bootstrap"
+import "./product.css"
+const Product = () => {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products")
+        const data = await response.json()
+        setProducts(data)
+      } catch (error) {
+        console.error("Error fetching products:", error)
+      }
+    }
+
+    fetchProducts()
+  }, [])
+
+  const cards = products.map((product) => (
+    <div className='col-md-3 mb-3'>
+      <Card style={{ width: "18rem" }}>
+        <div className='text-center'>
+          <Card.Img
+            variant='top'
+            src={product.image}
+            style={{ width: "100px", height: "130px" }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>{product.price}</Card.Text>
+          <Button variant='primary'>Add To Cart</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  ))
+
+  return (
+    <div>
+      <h1>Product List</h1>
+      <div className='row'>{cards}</div>
+    </div>
+  )
+}
+
+export default Product
+https://youtu.be/A_vRvDAZuOo?t=1093
