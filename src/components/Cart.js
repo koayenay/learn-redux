805d9f8544e95cce89c9de2d96a399675974c33b
koayenay <koayenay@gmail.com>
2023-05-30 10:08:54 -0700

    put and delete actions /cart working now

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,42 @@
 import React from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { Card } from "react-bootstrap"
+import { Button } from "react-bootstrap"
+import { remove } from "../store/cartSlice"
 
 const Cart = () => {
-  return <h2>Cart</h2>
+  const products = useSelector((state) => state.cart)
+  const dispatch = useDispatch()
+  const removeToCart = (id) => {
+    //dispatch a remove action
+    dispatch(remove(id))
+  }
+
+  const cards = products.map((product) => (
+    <div className='col-md-12 mb-3'>
+      <Card style={{ width: "18rem" }}>
+        <div className='text-center'>
+          <Card.Img
+            variant='top'
+            src={product.image}
+            style={{ width: "100px", height: "130px" }}
+          />
+        </div>
+        <Card.Body>
+          <Card.Title>{product.title}</Card.Title>
+          <Card.Text>{product.price}</Card.Text>
+        </Card.Body>
+
+        <Card.Footer>
+          <Button variant='danger' onClick={() => removeToCart(product.id)}>
+            Remove Item
+          </Button>
+        </Card.Footer>
+      </Card>
+    </div>
+  ))
+
+  return <div className='row'>{cards}</div>
 }
 
 export default Cart
--- src/components/NavBarPanel.js
+++ src/components/NavBarPanel.js
@@ -3,8 +3,10 @@ import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
 
 const NavBarPanel = () => {
+  const cartProducts = useSelector((state) => state.cart)
   return (
     <Navbar bg='light' expand='lg'>
       <Container fluid>
@@ -20,7 +22,7 @@ const NavBarPanel = () => {
         <Navbar.Collapse className='justify-content-end'>
           <Navbar.Text>
             <Nav.Link to='/cart' as={Link}>
-              My Bag 0
+              My Bag {cartProducts.length}
             </Nav.Link>
           </Navbar.Text>
         </Navbar.Collapse>
